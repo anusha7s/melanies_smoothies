b26ad243c2e4b21917c128208670949d5f78ec94
Anusha Saraswat <149650701+anusha7s@users.noreply.github.com>
2026-04-24 09:10:04 +0530

    Update Smoothiefroot API request and display data
    Removed initial request to Smoothiefroot API and added a new request to display data in a dataframe.

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,10 +2,6 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 import requests  
-smoothiefroot_response = requests.get(
-    "https://my.smoothiefroot.com/api/fruit/watermelon"
-) 
-st.text(smoothiefroot_response.json())
 # UI
 st.title(":cup_with_straw: Customize Your Smoothie :cup_with_straw:")
 st.write("Choose the fruits you want in your custom smoothie!")
@@ -49,3 +45,9 @@ if ingredients_list:
             ).collect()
 
             st.success(f'Your Smoothie is ordered, {name_on_order}!', icon="✅")
+
+smoothiefroot_response = requests.get(
+    "https://my.smoothiefroot.com/api/fruit/watermelon"
+) 
+# st.text(smoothiefroot_response.json())
+sf_df=st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
