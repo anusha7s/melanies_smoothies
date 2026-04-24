de142fee5fd358512c00b736cfe47060f683a519
Anusha Saraswat <149650701+anusha7s@users.noreply.github.com>
2026-04-24 09:05:37 +0530

    Fetch and display data from API
    Add API call to fetch data and display it.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,9 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
-
+import requests  
+smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
+st.text(smoothiefroot_response)
 # UI
 st.title(":cup_with_straw: Customize Your Smoothie :cup_with_straw:")
 st.write("Choose the fruits you want in your custom smoothie!")
